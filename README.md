2efc2ba22da46b8e04183086f8846dcfe7dc5bb4
Alexey Komarov <alex7kom@gmail.com>
2015-07-16 22:03:54 +0300

    0.0.1

---
+++ README.md
@@ -0,0 +1,27 @@
+# Steam Parental Features
+
+By far this features just parental unlock.
+
+## Installation
+
+```
+npm install steam-parental
+```
+
+## Usage
+
+Require it...
+
+```js
+var steamParental = require('steam-parental');
+```
+
+...then call:
+
+```js
+steamParental.parentalUnlock({ webCookie: cookies, PIN: pin }, callback);
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,39 @@
+var request = require('request');
+
+var steamDomain = 'https://steamcommunity.com';
+
+function parentalUnlock(options, callback) {
+  var cookieJar = request.jar();
+  var _request = request.defaults({ jar: cookieJar });
+
+  options.webCookie.forEach(function(name) {
+    cookieJar.setCookie(request.cookie(name), steamDomain);
+  });
+
+  _request.post({
+    uri: steamDomain + '/parental/ajaxunlock',
+    json: true,
+    headers: {
+      referer: steamDomain + '/'
+    },
+    form: {
+      pin: options.PIN
+    }
+  }, function(error, response, body) {
+    if (error || response.statusCode != 200) {
+      return callback(error || new Error(response.statusCode));
+    }
+    if (!body || typeof body.success != 'boolean') {
+      return callback(new Error('Invalid Response'));
+    }
+    if (!body.success) {
+      return callback(new Error('Incorrect PIN'));
+    }
+
+    callback();
+  });
+}
+
+module.exports = {
+  parentalUnlock: parentalUnlock
+};
